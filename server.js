41253619b7b8fd08116bb6b1c47056a20f24348f
panchepanevski <pance.panevski@gmail.com>
2019-12-04 13:39:25 +0100

    highscore

---
+++ server.js
@@ -0,0 +1,26 @@
+const MongoClient = require("mongodb").MongoClient;
+const assert = require("assert");
+
+// Connection URL
+const url = "mongodb://localhost:27017";
+
+// Database Name
+const dbName = "intro";
+
+// Create a new MongoClient
+const client = new MongoClient(url);
+
+// Use connect method to connect to the Server
+
+async function initDb() {
+  await client.connect();
+  console.log("Connected successfully to server");
+  const db = client.db(dbName);
+
+  const cursor = db.collection("users").find();
+  const users = await cursor.toArray();
+  console.log(users);
+  client.close();
+}
+
+initDb();
